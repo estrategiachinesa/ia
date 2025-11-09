40251effc1ca9e7ae5729afe9260de1147afc84a
Estratégia Chinesa <estrategiachinesa@gmail.com>
2025-11-09 22:56:04 +0000

    File changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/i.a..',
+  assetPrefix: '/i.a../',
   typescript: {
     ignoreBuildErrors: true,
   },

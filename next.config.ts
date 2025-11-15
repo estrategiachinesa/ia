945a7c9ff6912c5a1ae0935ecd718c49a8bbcabe
Estratégia Chinesa <estrategiachinesa@gmail.com>
2025-11-15 07:46:16 +0000

    File changes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import type {NextConfig} from 'next';
 
-// This is a dynamic Next.js project configured for Firebase App Hosting.
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,

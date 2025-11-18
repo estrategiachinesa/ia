2f40db591bafc7e3756d72ccb4b26618269c3bf9
Estratégia Chinesa <estrategiachinesa@gmail.com>
2025-11-18 21:42:36 +0000

    crie uma pagina chamada "/sessaochinesa"
    Crie uma nova página em um pro

---
+++ src/app/sessaochinesa/layout.tsx
@@ -0,0 +1,15 @@
+import { FirebaseClientProvider } from '@/firebase';
+
+export default function SessaoChinesaLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <FirebaseClientProvider>
+        <div className="theme-premium">
+            {children}
+        </div>
+    </FirebaseClientProvider>
+  );
+}

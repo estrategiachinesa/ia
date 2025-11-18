c03458a18d93a512adc6a9f057a508d4a865c346
Estratégia Chinesa <estrategiachinesa@gmail.com>
2025-11-18 21:26:15 +0000

    Try fixing this error: `Runtime Error: Error: useAppConfig must be used

--- src/app/demo/layout.tsx
+++ src/app/demo/layout.tsx
@@ -1,4 +1,5 @@
 
+import { FirebaseClientProvider } from '@/firebase';
 import { cn } from '@/lib/utils';
 
 export default function DemoLayout({
@@ -6,5 +7,9 @@ export default function DemoLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="theme-free">{children}</div>;
+  return (
+    <FirebaseClientProvider>
+      <div className="theme-free">{children}</div>
+    </FirebaseClientProvider>
+  );
 }

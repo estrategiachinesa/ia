c6e6dde7892b91ef00187dd1df3cbfbc640ea905
Estratégia Chinesa <estrategiachinesa@gmail.com>
2025-11-19 03:54:36 +0000

    o deploy atual nao funcionou, o ultimo que deu certo foi 019114e
    arrume

--- src/app/vip/layout.tsx
+++ src/app/vip/layout.tsx
@@ -1,4 +1,5 @@
 
+import { FirebaseClientProvider } from '@/firebase';
 import { cn } from '@/lib/utils';
 
 export default function VipLayout({
@@ -7,10 +8,12 @@ export default function VipLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="theme-premium">
-      <div className="fixed inset-0 -z-20 h-full w-full grid-bg" />
-      <div className="fixed inset-0 -z-10 bg-gradient-to-br from-background via-background/80 to-background" />
-      {children}
-    </div>
+    <FirebaseClientProvider>
+      <div className="theme-premium">
+        <div className="fixed inset-0 -z-20 h-full w-full grid-bg" />
+        <div className="fixed inset-0 -z-10 bg-gradient-to-br from-background via-background/80 to-background" />
+        {children}
+      </div>
+    </FirebaseClientProvider>
   );
 }
